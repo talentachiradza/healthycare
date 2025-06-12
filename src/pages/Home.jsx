c853a47e96e0bc5c110a963905f87f626303475c
Talent A Chiradza <talentachiradza@gmail.com>
2025-06-12 16:32:03 +0200

    Fix import paths for Linux/Vercel case sensitivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ import {BsArrowRight} from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
-import FaqList from '../components/Faq/faqList';
+import FaqList from '../components/Faq/FaqList';
 import Testimonial from '../components/Testimonial/Testimonial';
 
 
